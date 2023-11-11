ac5ad768b5d96746c113f3125c1b066b539b8464
yanyunwu <2480721346@qq.com>
2023-11-11 20:16:21 +0800

    init

---
+++ nest-app/src/events/events.gateway.ts
@@ -0,0 +1,47 @@
+import {
+    ConnectedSocket,
+    MessageBody,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+    WsResponse,
+    OnGatewayDisconnect,
+    OnGatewayConnection
+  } from '@nestjs/websockets';
+  import { from, Observable } from 'rxjs';
+  import { map } from 'rxjs/operators';
+  import { Server, Socket } from 'socket.io';
+  
+  @WebSocketGateway({
+    cors: {
+      origin: '*',
+    },
+  })
+  export class EventsGateway implements OnGatewayConnection,OnGatewayDisconnect {
+    handleDisconnect(client: any) {
+        console.log('un ', client.id)
+    }
+    handleConnection(client: Socket, ...args: any[]) {
+        console.log('client', client.id);
+    }
+
+    
+
+    @WebSocketServer()
+    server: Server;
+
+    @SubscribeMessage('newMessage')
+    handleMessage(@MessageBody() body: any, @ConnectedSocket() client: Socket,) {
+            console.log('client', client);
+    }
+
+    @SubscribeMessage('events')
+    findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+      return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+    }
+  
+    @SubscribeMessage('identity')
+    async identity(@MessageBody() data: number): Promise<number> {
+      return data;
+    }
+  }
