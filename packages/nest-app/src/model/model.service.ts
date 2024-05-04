b86ad923fbef98af6862aaa6443238216e239a02
yanyunwu <2480721346@qq.com>
2024-05-05 04:32:12 +0800

    feat: 新增chatgpt

--- packages/nest-app/src/model/model.service.ts
+++ packages/nest-app/src/model/model.service.ts
@@ -4,6 +4,9 @@ import * as https from 'https';
 import { TOKEN } from './consts';
 import { ConfigService } from 'src/modules/config/config.service';
 
+const sleep = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time));
+
 @Injectable()
 export class ModelService {
   constructor(private configService: ConfigService) {}
@@ -21,30 +24,45 @@ export class ModelService {
 
     console.log('messagesmessages', messages);
 
-    return axios.post(
-      'https://api.aigcbest.top/v1/chat/completions',
-      {
-        model: config?.model || 'gpt-3.5-turbo',
-        messages: [
-          ...messages,
-          {
-            role: 'user',
-            content: message,
-          },
-        ],
-        temperature: config?.temperature ?? 0.7,
-        stream: stream ?? true,
-        max_tokens: config?.maxTokens ?? undefined,
-      },
-      {
-        responseType: stream ? 'stream' : 'json',
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-          Connection: 'keep-alive',
+    const request = () =>
+      axios.post(
+        'https://api.aigcbest.top/v1/chat/completions',
+        {
+          model: config?.model || 'gpt-3.5-turbo',
+          messages: [
+            ...messages,
+            {
+              role: 'user',
+              content: message,
+            },
+          ],
+          temperature: config?.temperature ?? 0.7,
+          stream: stream ?? true,
+          max_tokens: config?.maxTokens ?? undefined,
         },
-        timeout: 10000,
-      },
-    );
+        {
+          responseType: stream ? 'stream' : 'json',
+          headers: {
+            Authorization: `Bearer ${TOKEN}`,
+            Connection: 'keep-alive',
+          },
+          timeout: 10000,
+        },
+      );
+
+    const retry = async () => {
+      try {
+        const r = await request();
+        return r;
+      } catch (err) {
+        await sleep(100);
+        const r = await retry();
+        return r;
+      }
+    };
+
+    const r = await retry();
+    return r;
   }
 
   getModels() {
